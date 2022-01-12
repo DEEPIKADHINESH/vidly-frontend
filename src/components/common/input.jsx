eb6400b3e122e2b9880a08e65aa7982ed30c1226
DEEPIKADHINESH <adeepika78@gmail.com>
2022-01-12 11:20:09 +0530

    basic validation

--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-const Input=({name,label,value,onChange})=>{
+const Input=({name,label,value,onChange,error})=>{
     return(
     <div className="form-group">
     <label htmlFor={name}>{label}</label><br/>
@@ -11,7 +11,9 @@ const Input=({name,label,value,onChange})=>{
      type="text"
      className="form-control"
      ></input><br/>
+     {error && <div className="alert alert-danger">{error}</div>}
      </div>
+     
     )
 }
 export default Input;
--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -3,17 +3,43 @@ import {Link} from "react-router-dom";
 import Input from "./common/input";
 class Login extends Component{
     state={
-        data:{username:"",password:""}
+        data:{username:"",password:""},
+        errors:{}
     }
+    validate=()=>{
+        const errors={}
+        if(this.state.data.username.trim()==="")
+       errors.username="username is required"
+        if(this.state.data.password.trim()==="")
+       errors.password="password is required"
+        return Object.keys(errors).length===0 ? null :errors
+    }
+
     handleSubmit=(e)=>{
-        e.preventDefault()
+        e.preventDefault();
+        const errors=this.validate();
+        this.setState({errors: errors||{}})
+        //here {} is used in errors setstate bz the error propertyalways want object if it is not set when password is onlypased then the page rerender 
+        //in console cannot read property of null
+        if (errors) return;
         console.log("submitted")
-        this.setState({e})
     }
-    handleChange=e=>{
+    validateProperty=(input)=>{
+     if(input.name==="username"){
+         if(input.value.trim()==="") return "Username is required"
+     }
+     if(input.name==="password"){
+        if(input.value.trim()==="") return "Password is required"
+    }
+    }
+    handleChange=({currentTarget:input})=>{
+        const errors={...this.state.errors};
+        const errorMessage=this.validateProperty(input)
+        if(errorMessage) errors[input.name]=errorMessage;
+        else delete errors[input.name];
         const data={...this.state.data}
-        data[e.currentTarget.name]=e.currentTarget.value;
-        this.setState({data})
+        data[input.name]=input.value;
+        this.setState({data,errors})
     }
     render(){
         return(
@@ -25,13 +51,16 @@ class Login extends Component{
               label="Username"
               onChange={this.handleChange}
               value={this.state.data.username}
+              error={this.state.errors.username}
              />
             <Input
               name="password"
               label="Password"
               onChange={this.handleChange}
-              value={this.state.data.password}/>
-               <Link to="/movies"><button className="btn btn-primary">Login</button></Link>
+              value={this.state.data.password}
+              error={this.state.errors.password}/>
+               {/* <Link to="/movies"><button className="btn btn-primary">Login</button></Link> */}
+               <button className="btn btn-primary">Login</button>
              </form>
           </div>
         )
