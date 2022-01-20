5d95de557b20d9141ead5032db0f82b791b9618d
DEEPIKADHINESH <adeepika78@gmail.com>
2022-01-20 10:36:30 +0530

    created commmon form for validate

---
+++ src/components/common/form.jsx
@@ -0,0 +1,42 @@
+import React,{Component}from "react";
+import Joi from "joi-browser"
+class Form extends Component{
+   state={
+       data:{},
+       errors:{}
+   }
+   validate=()=>{
+     const {error}=Joi.validate(this.state.data,this.schema,{abortEarly:false})
+     if(!error) return null;
+     const errors={}
+     for(let item of error.details)
+     errors[item.path[0]]=item.message;
+     return errors;
+   }
+   validateProperty=(input)=>{
+          const obj={[input.name]:[input.value]}
+          const schema={[input.name]:this.schema[input.value]}
+          const {error}=Joi.validate(obj,schema)
+          return error?error.details[0].message:null
+}
+handleChange=({currentTarget:input})=>{
+    const errors={...this.state.errors}
+    const errorMessage=this.validateProperty(input)
+    if(errorMessage) errors[input.name]=errorMessage
+    else delete errors[input.name]
+    const data={...this.state.data}
+    data[input.name]=[input.value]
+    this.setState({data,errors})
+}
+handleSubmit=(e)=>{
+    e.preventDefault();
+    const errors=this.validate();
+    this.setState({errors:errors||{}})
+      //here {} is used in errors setstate bz the error propertyalways want object if it is not set when password is onlypased then the page rerender 
+        //in console cannot read property of null
+    if(errors) return
+    this.doSubmit()
+}
+
+}
+export default Form
--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,7 +1,8 @@
 import React,{Component} from "react";
 import Joi from "joi-browser";
 import Input from "./common/input";
-class Login extends Component{
+import Form from "./common/form"
+class Login extends Form{
     state={
         data:{username:"",password:""},
         errors:{}
@@ -10,42 +11,10 @@ class Login extends Component{
         username:Joi.string().required().label("Username"),
         password:Joi.string().required().label("Password")
     }
-    validate=()=>{
-        const {error}=Joi.validate(this.state.data,this.schema,{abortEarly:false});
-        if(!error) return null;
-        //here path and message comes from joi to see this use console.log(result)
-        const errors={}
-        for(let item of error.details)
-        errors[item.path[0]]=item.message;
-        return errors;
-    }
-
-    handleSubmit=(e)=>{
-        e.preventDefault();
-        const errors=this.validate();
-        this.setState({errors: errors||{}})
-        //here {} is used in errors setstate bz the error propertyalways want object if it is not set when password is onlypased then the page rerender 
-        //in console cannot read property of null
-        if (errors) return;
+ doSubmit=()=>{
         console.log("submitted")
     }
-    validateProperty=(input)=>{
-     const obj={[input.name]:input.value}
-     const schema={[input.name]:this.schema[input.name]}
-   const {error}=  Joi.validate(obj,schema)
-   return error?error.details[0].message:null;
-
-    }
-    handleChange=({currentTarget:input})=>{
-        const errors={...this.state.errors};
-        const errorMessage=this.validateProperty(input)
-        if(errorMessage) errors[input.name]=errorMessage;
-        else delete errors[input.name];
-        const data={...this.state.data}
-        data[input.name]=input.value;
-        this.setState({data,errors})
-    }
-    render(){
+render(){
         return(
             <div>
                 <h1>Login</h1>
@@ -64,7 +33,7 @@ class Login extends Component{
               value={this.state.data.password}
               error={this.state.errors.password}/>
                {/* <Link to="/movies"><button className="btn btn-primary">Login</button></Link> */}
-               <button className="btn btn-primary">Login</button>
+               <button disabled={this.validate()}className="btn btn-primary">Login</button>
              </form>
           </div>
         )
