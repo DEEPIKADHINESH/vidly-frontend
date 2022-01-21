9058139dffd1348ea5f6eace3b0261b210ec52fe
DEEPIKADHINESH <adeepika78@gmail.com>
2022-01-21 15:36:07 +0530

    created search page

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -7,12 +7,15 @@ import {getGenres} from "../StarterCode/Starter Code/services/fakeGenreService";
 import MoviesTable from "./moviesTable";
 import {Link} from "react-router-dom";
 import _ from "lodash";
+import SearchBox from "./common/searchBox";
 class Movies extends Component{
     state={
         movies:[],
         genres:[],
         currentPage:1,
         pageSize:4,
+        searchQuery:"",
+    selectedGenre: null,
         sortColumn:{path:"title",order:"asc"}
     }
     componentDidMount(){
@@ -34,15 +37,23 @@ handleDelete=(movie)=>{
         this.setState({currentPage:page})
     }
     handleGenreSelect=(genre)=>{
-        this.setState({selectedGenre:genre,currentPage:1})
+        this.setState({selectedGenre:genre,searchQuery:"",currentPage:1})
     }
     handleSort=(sortColumn)=>{
         this.setState({sortColumn})
     }
+    handleSearch = query => {
+        this.setState({ searchQuery: query, selectedGenre: null, currentPage: 1 });
+      };
     getPagedData=()=>{
-        const {currentPage,pageSize,selectedGenre,movies:allMovies}=this.state
-        const filtered=selectedGenre && selectedGenre._id ?
-        allMovies.filter(m=>m.genre._id === selectedGenre._id):allMovies
+        const {currentPage,pageSize,selectedGenre,movies:allMovies,searchQuery}=this.state
+        let filtered=allMovies;
+        // const filtered=selectedGenre && selectedGenre._id ?
+        // allMovies.filter(m=>m.genre._id === selectedGenre._id):allMovies
+        if(searchQuery)
+        filtered=allMovies.filter(m=>m.title.toLowerCase().startsWith(searchQuery.toLowerCase()));
+        else if(selectedGenre && selectedGenre._id)
+        filtered= allMovies.filter(m=>m.genre._id === selectedGenre._id)
         const sorted=_.orderBy(filtered,[this.state.sortColumn.path],[this.state.sortColumn.order])
         const movies=paginate(sorted,currentPage,pageSize);
         return {totalCount:filtered,data:movies}
@@ -68,6 +79,7 @@ render(){
         <div className="col">
        <p>Showing {totalCount.length} movies in db</p>
        <Link to="/movies/new" className="btn btn-primary">NEW MOVIE</Link>
+       <SearchBox value={this.state.searchQuery} onChange={this.handleSearch} />
 <MoviesTable movies={movies}
              onLike={this.handleLike}
               onDelete={this.handleDelete}
